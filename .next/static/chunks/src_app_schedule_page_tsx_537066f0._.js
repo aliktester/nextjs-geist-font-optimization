f7903c2bd8d2377bb71e745d4fa0ba8c79dfeb19
aliktester <ruitele@gmail.com>
2025-07-18 20:50:04 +0200

    Add .next/static/chunks/src_app_schedule_page_tsx_537066f0._.js 1752864630171

---
+++ .next/static/chunks/src_app_schedule_page_tsx_537066f0._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_1becdf92._.js",
+  "static/chunks/node_modules_69f92d00._.js"
+],
+    source: "dynamic"
+});
